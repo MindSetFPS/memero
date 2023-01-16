50042bb50124d2dbf62db46dfd1da501de057a82
DanieEscobar <daanielescobar@gmail.com>
2023-01-15 19:47:56 -0600

    A generic delete button.

---
+++ frontend/src/lib/DeleteButton.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { createEventDispatcher } from "svelte"
+    import Button from "./Button.svelte";
+    export let id;
+
+    const dispatch = createEventDispatcher()
+
+    function handler(){
+        dispatch('delete', {
+            id: id
+        })
+    }
+</script>
+
+<Button type="red" on:clicked={handler} text="Borrar para siempre" />
