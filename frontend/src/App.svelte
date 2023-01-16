98eb378ee6e97825fe7cc674b3d302374dea7a05
DanieEscobar <daanielescobar@gmail.com>
2023-01-15 18:19:23 -0600

    Improve responsiveness.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -50,7 +50,7 @@
   getImages()
 </script>
 
-<main>
+<main class="p-4">
   <h1 class="text-5xl font-bold">Your memes sir</h1>
   <div>
     <input 
@@ -79,7 +79,15 @@
     {#await promise}
       <h1>Loading</h1>
     {:then promise}
-      <div class="grid grid-cols-3 gap-3">
+      <div 
+        class="
+          grid gap-3
+          grid-cols-3
+          md:grid-cols-4
+          lg:grid-cols-5
+          xl:grid-cols-6
+          2xl:grid-cols-8
+      ">
         {#each promise as post}
           <Meme filteredSearch={filteredSearch} post={post}/>
         {/each}
