f4d43ff0c9fe098e01d76105587df4b1838d0bcb
DanieEscobar <daanielescobar@gmail.com>
2023-01-15 19:47:29 -0600

    A generic button.

---
+++ frontend/src/lib/Button.svelte
@@ -0,0 +1,22 @@
+<button class="rounded-lg text-white p-2 hover:{colors[type]}-800 {colors[type]}-500" on:click={handler}>
+    {text}
+</button>
+
+<script>
+    import { createEventDispatcher } from "svelte"
+
+    export let type;
+    export let text;
+
+    let colors = {
+        red:"bg-red",
+        green:"bg-green",
+        blue:"bg-blue"
+    }
+
+    const dispatch = createEventDispatcher()
+
+    function handler(){
+        dispatch('clicked')
+    }
+</script>
