7cb9e19ba50f289601ba1900a865a6e23f641b99
DanieEscobar <daanielescobar@gmail.com>
2023-01-17 16:46:57 -0600

    Edit tags locally and send tags to server

--- frontend/src/lib/TagEditor.svelte
+++ frontend/src/lib/TagEditor.svelte
@@ -1,22 +1,48 @@
 <div>
-    TagEditor
-    <input type="text" name="tag" id="tag" on:keypress={addTag} bind:value={value} >
-
+    <input 
+        class="w-auto font-normal text-base hover:bg-slate-100 rounded-md transition"
+        type="text" 
+        name="tag" 
+        id="tag" 
+        placeholder="agrega un nuevo tag" 
+        on:keypress={addTag} 
+        bind:value={value}
+    >
 </div>
 
 <script>
+    import { memesStore } from "./store";
     import getBaseUrl from "./getBaseUrl";
     export let id;
+    export let index;
     let value;
 
     let baseUrl = getBaseUrl()
 
     async function addTag(event){
         if(event.keyCode == 13){
+
+            //copy the current list of memes
+            const memeIndex = index
+            let memesCopy = [...$memesStore]
+
+            //edit the copy
+
+            let toArray = value.split(",")
+            let toString_ = toArray.toString()
+
+            memesCopy[memeIndex].tags = toString_  + ',' + $memesStore[memeIndex].tags
+            // console.log(memesCopy[memeIndex].tags)
+
+            //replace with the copy
+            memesStore.update((value) =>  value = memesCopy)
+
+            value = ''
+
             const res = await fetch(`${baseUrl}/image/edit/${id}`, {
                 method: "POST",
                 body: JSON.stringify({
-                    tags: value
+                    tags: memesCopy[memeIndex].tags
                 }),
                 headers: {
                     "Content-type": "application/json; charset=UTF-8"
@@ -25,7 +51,10 @@
             const text = await res.json();
 
             console.log(text)
-            
         }
+    }
+
+    function handleTagDeleted(){
+
     }
 </script>
