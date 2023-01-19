8783b047050b120451374c760bee7f87f9318971
DanieEscobar <daanielescobar@gmail.com>
2023-01-19 14:32:10 -0600

    move bigPicture to its own component

--- frontend/src/lib/Meme.svelte
+++ frontend/src/lib/Meme.svelte
@@ -1,122 +1,43 @@
 {#if filteredSearch.length == 0 || matchingElements.length > 0}
 <div class="max-w-md" >
-    <img src="{baseUrl}/image/{post.filename}" alt="" on:click={bigPicture}>
+    <img src="{baseUrl}/image/{post.filename}" alt="" on:click={ () => {
+        isBigPicture = !isBigPicture
+        console.log(isBigPicture)
+        }}>
     <div>
         <p class="whitespace-nowrap overflow-hidden overflow-ellipsis">
             {post.filename}
         </p>
         <TagList tags={$memesStore[index].tags} on:deleteTag={handleTagDeleted} />
     </div>
-    {#if isBigPicture }
-        <div class="fixed h-screen w-screen top-0 left-0 z-20" >
-            <div 
-                class="
-                    absolute z-20 top-0 
-                    bg-white
-                    h-1/2 w-1/2
-                    left-1/2 -translate-x-1/2 translate-y-1/2
-                    p-2 rounded-lg
-                    text-center
-                    lg:flex
-                    lg:justify-start
-                    scale-125
-            ">
-                <!-- <div>                   left-0  </div> -->
-                <div 
-                    class="h-full w-1/3 bg-contain bg-center bg-no-repeat " 
-                    style="background-image: url({baseUrl}/image/{post.filename})"
-                >
-                </div>
-                <div class="flex flex-col justify-center font-bold w-full h-full p-2">
-                    <div class="flex w-full align-middle items-center my-2">
-                        <div class="text-lg inline whitespace-nowrap">
-                            Agregar tags
-                        </div>
-                        <TagEditor id={post.id} index={index} />
-                    </div>
-                    <div class="overflow-auto" >
-                        <TagList tags={$memesStore[index].tags} on:deleteTag={handleTagDeleted} />
-                    </div>
-                    {#if showconfirmDelete}
-                        De verdad quieres borrar este meme? Esto no se puede deshacer
-                        <DeleteButton id={post.id} on:delete={deleteMeme} />
-                    {:else}
-                        <Button on:clicked={ () => showconfirmDelete = !showconfirmDelete} type="red" text="Borrar meme"   />
-                    {/if}
-                </div>
-            </div>
-            <GrayBackground on:clicked={bigPicture} />
-        </div>
+    <MemeDetails 
+        on:deleteTag={handleTagDeleted} 
+        on:exitBigPicture={ () => isBigPicture = !isBigPicture} 
+        index={index} 
+        id={post.id}
+        post={post}
+        isBigPicture={isBigPicture}
+        />
+    </div>
     {/if}
-</div>
-{/if}
-
-<script>
+    
+    <script>
     import { createEventDispatcher } from "svelte"
-    import GrayBackground from "./GrayBackground.svelte";
-    import Button from "./Button.svelte";
-    import DeleteButton from "./DeleteButton.svelte";
     import TagList from "./TagList.svelte";
     import getBaseUrl from "./getBaseUrl";
-    import TagEditor from "./TagEditor.svelte";
     import { memesStore } from "./store";
+    import MemeDetails from "./MemeDetails.svelte";
 
     export let post;
     export let index;
     export let filteredSearch;
     let matchingElements
     let isBigPicture = false;
-    let showconfirmDelete = false;
     const dispatch = createEventDispatcher()
-
     let baseUrl = getBaseUrl()
+
     $: {
         matchingElements = filteredSearch.filter(element => post.tags.includes(element))
-    }
-
-    function bigPicture(){
-        isBigPicture = !isBigPicture
-        showconfirmDelete = false
-
-
-        //copy the current list of memes
-        let memesCopy = [...$memesStore]
-
-        console.log(memesCopy)
-
-        //sort by tag length
-        memesCopy.sort( (meme1, meme2) => {
-            let tags1 = meme1.tags.length
-            let tags2 = meme2.tags.length
-            
-            if(tags1 < tags2){
-                return 1
-            }
-
-            if(tags1 > tags2){
-                return -1
-            }
-
-            return 0
-        })
-
-        //replace with the copy
-        memesStore.update((value) =>  value = memesCopy)
-
-    }
-
-    async function deleteMeme(event){
-        const res = await fetch(`${baseUrl}/image/delete/${event.detail.id}`);
-		const text = await res.json();
-    
-		if (res.ok) {
-            dispatch('meme_deleted', {
-                index: index
-            })
-            isBigPicture = !isBigPicture
-		} else {
-      throw new Error(text);
-		}
     }
 
     function handleTagDeleted(event){
---
+++ frontend/src/lib/MemeDetails.svelte
@@ -0,0 +1,133 @@
+<script>
+    import { createEventDispatcher } from "svelte"
+    import GrayBackground from "./GrayBackground.svelte";
+    import Button from "./Button.svelte";
+    import DeleteButton from "./DeleteButton.svelte";
+    import TagList from "./TagList.svelte";
+    import getBaseUrl from "./getBaseUrl";
+    import TagEditor from "./TagEditor.svelte";
+    import { memesStore } from "./store";
+
+    export let post;
+    export let id;
+    export let index;
+    export let isBigPicture;
+    let showconfirmDelete = false;
+    const dispatch = createEventDispatcher()
+    let baseUrl = getBaseUrl()
+
+    function bigPicture(){
+        if(isBigPicture){
+            dispatch("exitBigPicture")
+        }
+        showconfirmDelete = false
+
+        //copy the current list of memes
+        let memesCopy = [...$memesStore]
+
+        //sort by tag length
+        memesCopy.sort( (meme1, meme2) => {
+            let tags1 = meme1.tags.length
+            let tags2 = meme2.tags.length
+            
+            if(tags1 < tags2){
+                return 1
+            }
+            if(tags1 > tags2){
+                return -1
+            }
+            return 0
+        })
+        //replace with the copy
+        memesStore.update((value) =>  value = memesCopy)
+    }
+
+    async function deleteMeme(event){
+        const res = await fetch(`${baseUrl}/image/delete/${event.detail.id}`);
+		const text = await res.json();
+    
+		if (res.ok) {
+
+            //copy the current list of memes
+            const memeIndex = index
+            console.log(memeIndex)
+            let memesCopy = [...$memesStore]
+
+            //edit the copy
+            memesCopy.splice(memeIndex, 1)
+
+            //replace with the copy
+            memesStore.update((value) =>  value = memesCopy)
+            isBigPicture = !isBigPicture
+		} else {
+      throw new Error(text);
+		}
+    }
+
+    async function handleTagDeleted(event){
+
+        const memeIndex = index
+        //copy the current list of memes
+        let memesCopy = [...$memesStore]
+
+        //edit the copy
+        memesCopy[memeIndex].tags = event.detail.taglist.toString()
+
+        //replace with the copy
+        memesStore.update((value) =>  value = memesCopy)
+
+
+        //  edit in db
+        const res = await fetch(`${baseUrl}/image/edit/${id}`, {
+                method: "POST",
+                body: JSON.stringify({
+                    tags: memesCopy[memeIndex].tags
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            });
+            const text = await res.json();
+
+
+    }
+</script>
+
+{#if isBigPicture }
+<div class="fixed h-screen w-screen top-0 left-0 z-20"  >
+    <div 
+        class="
+            absolute z-20 top-0 
+            text-center bg-white p-2 rounded-lg scale-125
+            h-1/2 w-1/2
+            left-1/2 -translate-x-1/2 translate-y-1/2
+            lg:flex lg:justify-start
+    ">
+        <!-- <div>                   left-0  </div> -->
+        <div 
+            class="h-full w-1/3 bg-contain bg-center bg-no-repeat " 
+            style="background-image: url({baseUrl}/image/{post.filename})"
+        >
+        </div>
+        <div class="flex flex-col justify-center font-bold w-full h-full p-2">
+            {index}
+            <div class="flex w-full align-middle items-center my-2">
+                <div class="text-lg inline whitespace-nowrap">
+                    Agregar tags
+                </div>
+                <TagEditor id={post.id} index={index} />
+            </div>
+            <div class="overflow-auto" >
+                <TagList tags={$memesStore[index].tags} on:deleteTag={handleTagDeleted} />
+            </div>
+            {#if showconfirmDelete}
+                De verdad quieres borrar este meme? Esto no se puede deshacer
+                <DeleteButton id={post.id} on:delete={deleteMeme} />
+            {:else}
+                <Button on:clicked={ () => showconfirmDelete = !showconfirmDelete} type="red" text="Borrar meme"   />
+            {/if}
+        </div>
+    </div>
+    <GrayBackground on:clicked={bigPicture}/>
+</div>
+{/if}
