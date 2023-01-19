64c66165422ca7ff4a1e73794f57ce344675e415
DanieEscobar <daanielescobar@gmail.com>
2023-01-19 15:49:39 -0600

    remove empty strings, white spaces, and random commas, it will only add a tag if it has actual content

--- frontend/src/lib/TagEditor.svelte
+++ frontend/src/lib/TagEditor.svelte
@@ -25,12 +25,28 @@
             let memesCopy = [...$memesStore]
 
             //edit the copy
-
             let toArray = value.split(",")
-            let toString_ = toArray.toString()
+            let cleanArray = []
 
-            memesCopy[memeIndex].tags = toString_  + ',' + $memesStore[memeIndex].tags
-            // console.log(memesCopy[memeIndex].tags)
+            // remove whitespace at the end and begining of each tag
+            toArray.forEach( word => {
+                let cleanWord = word.trim()
+                // check if word is empty
+                if(word.length == 0){
+                    return
+                }
+                // check if strng if made of just white spaces
+                if (!word.replace(/\s/g, '').length) {
+                    return
+                }
+                cleanArray.push(cleanWord)
+            });
+            
+            let cleanArraytoString = cleanArray.toString()
+
+            // remove annoying comma at the end
+            let conditionalSum = $memesStore[memeIndex].tags.length > 0 ? ',' + $memesStore[memeIndex].tags : ''
+            memesCopy[memeIndex].tags = cleanArraytoString  + conditionalSum
 
             //replace with the copy
             memesStore.update((value) =>  value = memesCopy)
@@ -47,12 +63,6 @@
                 }
             });
             const text = await res.json();
-
-            console.log(text)
         }
-    }
-
-    function handleTagDeleted(){
-
     }
 </script>
