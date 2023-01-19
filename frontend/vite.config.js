92387132b4bb962e14f31cb2a1bd20942ee4cc40
DanieEscobar <daanielescobar@gmail.com>
2023-01-19 14:38:26 -0600

    add emptyOutDir option

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: '/app/public'
+    outDir: '/app/public',
+    emptyOutDir: true
   }
 })
