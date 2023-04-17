d043e116612d1017a0daee050d9a04002dd1d3bd
DanieEscobar <daanielescobar@gmail.com>
2023-04-16 23:58:16 -0600

    Optimization: virtual scrolling, remove elements from dom when they are out of screen

--- frontend/src/lib/Meme.svelte
+++ frontend/src/lib/Meme.svelte
@@ -4,6 +4,7 @@
     import getBaseUrl from "./getBaseUrl";
     import { memesStore } from "./store";
     import MemeDetails from "./MemeDetails.svelte";
+    import IntersectionObserver from "svelte-intersection-observer"
 
     export let post;
     export let index;
@@ -12,6 +13,8 @@
     let isBigPicture = false;
     const dispatch = createEventDispatcher()
     let baseUrl = getBaseUrl()
+    let element;
+    let isOnScreen;
 
     $: {
         matchingElements = filteredSearch.filter(element => post.tags.includes(element))
@@ -29,25 +32,38 @@
 </script>
 
 {#if filteredSearch.length == 0 || matchingElements.length > 0}
-    <div class="w-80 h-80 p-2" >
-        <img class="object-cover h-56 w-80 hover:object-scale-down hover:mx-auto z-10 hover:z-100 transition" src="{baseUrl}/image/file/{post.filename}" alt="" on:click={ () => {
-            isBigPicture = !isBigPicture
-            console.log(isBigPicture)
-            }}>
-        <div class="h-24" >
-            <p class="whitespace-nowrap overflow-hidden overflow-ellipsis">
-                {post.filename}
-            </p>
-            <TagList tags={$memesStore[index].tags} on:deleteTag={handleTagDeleted} />
+<IntersectionObserver {element} threshold={0.1}
+    on:observe={(e) => {
+        console.log(e.detail.isIntersecting)
+        isOnScreen = e.detail.isIntersecting
+    }}
+>
+    <div class="w-80 h-80 p-2" bind:this={element} >
+            {#if isOnScreen }
+                <div>
+                    <img class="object-cover h-56 w-80 hover:object-scale-down hover:mx-auto z-10 hover:z-100 transition" src="{baseUrl}/image/file/{post.filename}" alt="" on:click={ () => {
+                        isBigPicture = !isBigPicture
+                        console.log(isBigPicture)
+                    }}>
+                    <div class="h-24" >
+                        <p class="whitespace-nowrap overflow-hidden overflow-ellipsis">
+                            {post.filename}
+                        </p>
+                        <TagList tags={$memesStore[index].tags} on:deleteTag={handleTagDeleted} />
+                    </div>
+                    <MemeDetails 
+                    on:deleteTag={handleTagDeleted} 
+                    on:exitBigPicture={ () => isBigPicture = !isBigPicture} 
+                    index={index} 
+                    id={post.id}
+                    post={post}
+                    isBigPicture={isBigPicture}
+                    />
+                </div>
+            {:else}
+                <div></div>
+            {/if}
         </div>
-        <MemeDetails 
-            on:deleteTag={handleTagDeleted} 
-            on:exitBigPicture={ () => isBigPicture = !isBigPicture} 
-            index={index} 
-            id={post.id}
-            post={post}
-            isBigPicture={isBigPicture}
-        />
-    </div>
+        </IntersectionObserver>
 {/if}
     
