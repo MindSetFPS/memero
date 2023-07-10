cad318cae21e326e0c0f32d9a007880fb3772378
DanieEscobar <daanielescobar@gmail.com>
2023-07-09 23:32:52 -0600

    Preview images in upload component

--- frontend/src/lib/UploadMemes.svelte
+++ frontend/src/lib/UploadMemes.svelte
@@ -1,42 +1,4 @@
-<div 
-    class="shadow-lg rounded-xl p-2 fixed bottom-0 right-0 m-4 bg-white z-30" 
->
-    {#if isBubble}
-        <div on:click={ () => isBubble = !isBubble}
-            on:keypress={() => isBubble = !isBubble} 
-            class="text-4xl">
-            Subir un meme 🤡
-        </div>
-    {:else}
-        {#if isUploading}
-            <div class="animate-pulse">
-                Subiendo memes <span >🤡</span>
-            </div>
-        {/if}
-        {#if !isUploading}
-            <form
-            action="/" 
-            method="post" 
-            enctype="multipart/form-data" 
-            on:submit|preventDefault={handleUpload}
-                class=""
-            >
-            <div>
-                <input type="file" id="myFile" name="file" bind:files multiple >
-            </div>
-            <TagSetup bind:value={tags} on:tag={() => console.log(tags)} />
-                <div>
-                    <Button on:clicked={() => isBubble = true} type="green" text="Cerrar">
-                        Cerrar
-                    </Button>
-                    <input type="submit" value="Upload" class="bg-blue-500 hover:bg-blue-600 text-white">
-                </div>
-            </form>
-        {/if}
-    {/if}
-</div>
-
-  <script>
+<script>
     import TagSetup from "./TagSetup.svelte";
     import { createEventDispatcher } from "svelte"
     import Button from "./Button.svelte";
@@ -47,8 +9,22 @@
     let baseUrl = getBaseUrl()
     let isBubble = true
     let isUploading = false
+    let previews = new Array()
    
     let dispatch = createEventDispatcher()
+
+    // render previews 
+    function onFileSelected(e){
+        previews = []
+        for (let index = 0; index < files.length; index++){
+            let reader = new FileReader()
+            reader.readAsDataURL(files[index])
+            reader.onload = e => {
+                previews = [...previews, e.target.result]
+                console.log("pushed ", e.target.result)
+            }
+        }
+    }
 
     async function handleUpload(event){
         const formData = new FormData()
@@ -70,6 +46,56 @@
             dispatch('filesUploaded')
             event.target.reset()
             isUploading = false
+            previews = []
         }
     }
 </script>
+
+<div class="shadow-lg rounded-xl p-2 fixed bottom-0 right-0 m-4 bg-white z-20 cursor-pointer">
+    {#if isBubble}
+        <div on:click={ () => isBubble = !isBubble}
+            on:keypress={() => isBubble = !isBubble} 
+            class="text-4xl">
+            Subir un meme 🤡
+        </div>
+    {:else}
+        {#if isUploading}
+            <div class="animate-pulse">
+                Subiendo memes <span >🤡</span>
+            </div>
+        {/if}
+        {#if !isUploading}
+            <form
+                action="/" 
+                method="post" 
+                enctype="multipart/form-data" 
+                on:submit|preventDefault={handleUpload}
+            >
+                <div class="">
+                    <input 
+                        type="file" 
+                        id="myFile" 
+                        name="file" 
+                        bind:files 
+                        multiple 
+                        on:change={(e) => onFileSelected(e)} 
+                    >
+                </div>
+                {#if previews.length > 0}
+                    <div class="flex my-2">
+                        {#each previews as imageURL}
+                            <img src="{imageURL}" alt="" class="max-h-6">
+                        {/each}
+                    </div>
+                {/if}
+                <TagSetup bind:value={tags} on:tag={() => console.log(tags)} />
+                <div>
+                    <Button on:clicked={() => isBubble = true} type="green" text="Cerrar">
+                        Cerrar
+                    </Button>
+                    <input type="submit" value="Upload" class="bg-blue-500 hover:bg-blue-600 text-white p-2 rounded-md">
+                </div>
+            </form>
+        {/if}
+    {/if}
+</div>
