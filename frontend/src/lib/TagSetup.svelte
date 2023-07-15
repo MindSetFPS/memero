db08250fc76dd0d0542d886fabc059b7e7d8f7a5
DanieEscobar <daanielescobar@gmail.com>
2023-07-15 07:02:08 -0600

    improve styles

--- frontend/src/lib/TagSetup.svelte
+++ frontend/src/lib/TagSetup.svelte
@@ -13,11 +13,12 @@
     }
 
 </script>
-
-<input type="text" name="tag" id="tags" placeholder="agregar tags" bind:value={currentTag} />
-<button on:click|preventDefault={addTag} > Add </button>
-{#each value as tag}
+<div class="p-2" >
+    <input type="text" name="tag" id="tags" placeholder="agregar tags" bind:value={currentTag} />
+    <button on:click|preventDefault={addTag} class="p-2 bg-gray-200 hover:bg-gray-300 rounded-md" > Add </button>
+    {#each value as tag}
     <div class="">
         {tag}
     </div>
-{/each}
+    {/each}
+</div>
