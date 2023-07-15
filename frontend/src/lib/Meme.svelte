6d2e1173e51cac27de0b534ac96eb9a7edbdff84
DanieEscobar <daanielescobar@gmail.com>
2023-07-15 06:56:06 -0600

    change styles

--- frontend/src/lib/Meme.svelte
+++ frontend/src/lib/Meme.svelte
@@ -38,14 +38,39 @@
         isOnScreen = e.detail.isIntersecting
     }}
 >
-    <div class="w-80 h-80 p-2" bind:this={element} >
+    <div 
+        class="
+            w-full h-full
+            sm:w-48 sm:h-48
+            md:w-54 md:h-54
+            lg:w-54 lg:h-60
+            xl:w-52 xl:h-52
+            2xl:w-56 2xl:h-56
+            p-2
+        " 
+        bind:this={element} 
+    >
             {#if isOnScreen }
                 <div>
-                    <img class="object-cover h-56 w-80 hover:object-scale-down hover:mx-auto z-10 hover:z-100 transition" src="{baseUrl}/image/file/{post.filename}" alt="" on:click={ () => {
-                        isBigPicture = !isBigPicture
-                        console.log(isBigPicture)
-                    }}>
-                    <div class="h-24" >
+                    <img 
+                        class="
+                            object-cover
+                            sm:w-48 sm:h-28
+                            md:w-60 md:h-28
+                            lg:w-60 lg:h-40
+                            xl:w-52 xl:h-32
+                            2xl:w-60 2xl:h-40 
+                            hover:object-scale-down 
+                            hover:mx-auto 
+                            z-10 hover:z-100 transition" 
+                        src="{baseUrl}/image/file/{post.filename}" 
+                        alt="" 
+                        on:click={ () => {
+                            isBigPicture = !isBigPicture
+                            console.log(isBigPicture)
+                        }}
+                    >
+                    <div class="h-12 xl:h-12 2xl:h-8" >
                         <p class="whitespace-nowrap overflow-hidden overflow-ellipsis">
                             {post.filename}
                         </p>
