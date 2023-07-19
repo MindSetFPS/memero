98617cc93d66ab6223bf9936c3ea1d5e299f993a
MindSetFPS <45702604+MindSetFPS@users.noreply.github.com>
2023-07-19 15:25:44 -0600

    make build path relative instead of absolute (might introduce bugs)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: '/app/public',
+    outDir: '../public',
     emptyOutDir: true
   }
 })
