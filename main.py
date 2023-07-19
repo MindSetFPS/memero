2b42f17727302ee8f206605840442736c95500e4
MindSetFPS <45702604+MindSetFPS@users.noreply.github.com>
2023-07-19 15:14:46 -0600

    update window name

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import webview
 from server import app
 
 if __name__ == '__main__':
-    window = webview.create_window('Pyweb', app)
-    webview.start(debug=True)
+    window = webview.create_window('Memero', app)
+    webview.start(debug=False)
